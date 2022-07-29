e4b003d9031f80fb3e17272516c2eba280b0cdb9
Martin Muda <martinmuda@gmail.com>
2022-07-28 22:59:58 -0300

    feat: add npm name checker

--- dev_scripts/init.js
+++ dev_scripts/init.js
@@ -1,20 +1,34 @@
 import fs from 'fs'
 import { Command } from 'commander'
 import chalk from 'chalk'
+import npmName from 'npm-name'
 let projectName
 function init() {
     const program = new Command()
     .arguments('<project-name>')
     .usage(`${chalk.green('<project-name>')} [options]`)
-    .action(name => {
+    .action(async name => {
       projectName = name
       console.log(projectName)
-      fs.mkdirSync(`./${projectName}`)
-      fs.copyFile('./dev_scripts/README.md', `./${projectName}/README2.md`, (err) => {
-        if (err) throw err;
+      console.log(await npmName(projectName))
+      try {
+        await npmName(projectName)
+      } catch (error) {
+        console.log(error.message);
+        // Invalid package name: _ABC
+        // - name cannot start with an underscore
+        // - name can no longer contain capital letters
+      }
+      // check npm name
+      // if available console and create folder and keeop name
+      // else not avilable : change or no problem I dont  want to publish (overrride)
+      // 
+      //fs.mkdirSync(`./${projectName}`)
+      //fs.copyFile('./dev_scripts/README.md', `./${projectName}/README2.md`, (err) => {
+      //  if (err) throw err;
           
-        console.log('File Copy Successfully.');
-      });
+      //  console.log('File Copy Successfully.');
+      //});
     })
     .parse(process.argv)
 
