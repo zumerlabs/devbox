f75c85564de5f19389890834ce48f5e58ae70ad6
Martin Muda <martinmuda@gmail.com>
2022-07-29 00:23:23 -0300

    feat: ckeck if name exist on npm optimized

--- dev_scripts/init.js
+++ dev_scripts/init.js
@@ -2,6 +2,8 @@ import fs from 'fs'
 import { Command } from 'commander'
 import chalk from 'chalk'
 import npmName from 'npm-name'
+import { isTaken } from "is-name-taken"
+import validate from 'validate-npm-package-name'
 let projectName
 function init() {
     const program = new Command()
@@ -10,15 +12,22 @@ function init() {
     .action(async name => {
       projectName = name
       console.log(projectName)
-      console.log(await npmName(projectName))
+      console.log(validate(projectName))
       try {
-        await npmName(projectName)
+        console.log('checking availability...')
+        let getNPM = await fetch(`https://registry.npmjs.com/${projectName}`)
+        let result = await getNPM.json()
+        console.log(result.error ? 'Avialble' : 'taken')
       } catch (error) {
-        console.log(error.message);
+        console.log(error)
         // Invalid package name: _ABC
         // - name cannot start with an underscore
         // - name can no longer contain capital letters
+      } finally {
+
       }
+
+
       // check npm name
       // if available console and create folder and keeop name
       // else not avilable : change or no problem I dont  want to publish (overrride)
